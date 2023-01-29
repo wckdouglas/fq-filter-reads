2aa6314e8ac1d67b5fa2bec7745b9ad57608392d
Douglas Wu <wckdouglas@gmail.com>
2023-01-29 15:38:25 -0500

    added test script

--- src/lib.rs
+++ src/lib.rs
@@ -61,9 +61,7 @@ pub fn process_read(
     let read_in_list: bool = id_list.contains(&seq_id);
     if (read_in_list && !inverse) | (!read_in_list && inverse) {
         let record_string = record.to_string();
-        let record_fmt = record_string
-            .strip_suffix("\n")
-            .ok_or_else(|| "Bad record")?;
+        let record_fmt = record_string.strip_suffix('\n').ok_or("Bad record")?;
         println!("{}", record_fmt);
         out_count += 1;
     }
--- test_script.sh
+++ test_script.sh
@@ -1,6 +1,16 @@
+set -x
+
+# mock data
 mkdir -p tmp
 printf "@a\nAAA\n+\nAAA\n@c\nCCC\n+\nCCC\n@t\nTTT\n+\nTTT\n" | gzip > tmp/test.fq.gz 
 printf "a\nt\n" > tmp/id_list 
 printf "@a\nAAA\n+\nAAA\n@t\nTTT\n+\nTTT\n" > tmp/expected.fq  
+printf "@c\nCCC\n+\nCCC\n" > tmp/expected_inverse.fq  
+
+# run
 cargo run -- --in-fastq tmp/test.fq.gz --in-id-list tmp/id_list > tmp/out.fq 
 diff tmp/expected.fq tmp/out.fq > /dev/null; echo "exit code: $?"
+
+# run inverse
+cargo run -- --in-fastq tmp/test.fq.gz --in-id-list tmp/id_list --inverse > tmp/out.fq 
+diff tmp/expected_inverse.fq tmp/out.fq > /dev/null; echo "exit code: $?"
